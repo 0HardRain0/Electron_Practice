323b618fc280af409349aaa402fa557b9bf7a6a2
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2022-06-17 14:12:14 +0900

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,22 @@
+// All of the Node.js APIs are available in the preload process.
+
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }ß
+})
+
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system')
+})
+const { contextBridge, ipcRenderer } = require('electron')
